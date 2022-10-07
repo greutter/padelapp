08f398659b5bd8bdf92def8bacb6fb978648bcbe
Gonzalo Reutter <greutter@gmail.com>
2022-10-07 10:44:25 -0300

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   # console
+
+  before_action :redirect_to_domain
+
+  def redirect_to_domain
+    host = request.host
+    path = path = request.fullpath.split("?")[0]
+    if host == 'padelapp.cl' && path == "/"
+      redirect_to 'https://padelapp.cl'
+    end
+  end
 end
