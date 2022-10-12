27f704f5ccacbb163f52c7327df3a0fe704cb962
Gonzalo Reutter <greutter@gmail.com>
2022-10-12 12:56:32 -0300

    ....

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # console
 
   before_action :redirect_to_domain
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def redirect_to_domain
     host = request.host
@@ -10,4 +11,13 @@ class ApplicationController < ActionController::Base
       # redirect_to 'https://padelapp.cl'
     end
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone, :email])
+  end
+
+
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,12 +1,22 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
   def update_resource(resource, params)
-    if resource.provider == 'google_oauth2'
-      params.delete('current_password')
-      resource.password = params['password']
-      resource.update_without_password(params)
-    else
-      resource.update_with_password(params)
-    end
+    resource.update(params)
+  end
+
+
+  private
+
+  def resources_params
+    params.require(:user).permit(:first_name, :last_name, :email, :phone)
+  end
+
+  protected
+  def after_update_path_for(resource)
+     edit_registration_path(resource)
+  end
+
+  def after_sign_up_path_for(resource)
+    shares_path
   end
 end
