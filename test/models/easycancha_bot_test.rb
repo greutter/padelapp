17650228157d9d0fb722535587348883c4811c0d
Gonzalo Reutter <greutter@gmail.com>
2022-11-08 16:35:50 -0300

    ...

---
+++ test/models/easycancha_bot_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class EasycanchaBotTest < ActiveSupport::TestCase
+  def setup
+    
+  end
+
+  test "should get availability" do
+    EasycanchaBot.availability
+  end
+
+
+
+end
