8395ac345585746d383832f6abab1157d66e0b16
Gonzalo Reutter <greutter@gmail.com>
2022-11-08 18:38:26 -0300

    Easycancha bot haciendo reservas

--- app/bots/easycancha_bot.rb
+++ app/bots/easycancha_bot.rb
@@ -1,19 +1,18 @@
 class EasycanchaBot
-  def self.create_driver
+  include EasycanchaReservationService
+  def create_driver
     options = Selenium::WebDriver::Chrome::Options.new
     options.add_argument("--window-size=600,1200")
-    # options.add_argument('--headless')
-
+    options.add_argument("--headless")
     caps = Selenium::WebDriver::Remote::Capabilities.chrome
     caps.accept_insecure_certs = true
-
     @driver =
       Selenium::WebDriver.for :chrome, capabilities: caps, options: options
     @driver.manage.timeouts.implicit_wait = 5000
     return @driver
   end
 
-  def self.login(username: "reutter.carvajal@gmail.com", password: "ec1234")
+  def login(username: "reutter.carvajal@gmail.com", password: "ec1234")
     create_driver
     @driver.get "https://www.easycancha.com/book/search?lang=es-CL&country=CL"
     button =
@@ -37,13 +36,12 @@ class EasycanchaBot
     return @driver
   end
 
-
-  def self.availability(club_id: 502, date: Time.now + 1.days, timespan: 90)
+  def get_availability(club_id: 502, date: Time.now + 1.days, timespan: 90)
     url =
       "https://www.easycancha.com/api/sports/7/clubs/#{club_id}/timeslots?date=#{date.strftime "%Y-%m-%d"}&time=05:00:00&timespan=#{timespan}"
     create_driver
     begin
-      self.login()
+      login
       # agent = Mechanize.new
       # page = agent.get(url)
       @driver.get(url)
@@ -70,85 +68,7 @@ class EasycanchaBot
     end
   end
 
-  def self.reserve(club_id: 502, starts_at: DateTime.tomorrow, duration: 90)
-    begin
-      self.login()
-      self.put_separator
-      self.select_sport(club_id: club_id)
-      sleep(2)
-      self.select_duration(duration: 90)
-      sleep(2)
-      self.select_date()
-      sleep(2)
-      self.select_timeslot()
-      sleep(2)
-      self.screenshot
-    rescue Exception => e
-      puts e.message
-    ensure
-      @driver.close
-      @driver.quit
-      self.put_separator
-    end
-  end
-
-  def self.select_sport(club_id: 502)
-    begin
-      puts "Selecting sport..."
-      create_driver unless @driver
-      @driver.get "https://www.easycancha.com/book/clubs/#{club_id}/sports?lang=es-CL&country=CL"
-      sleep(2)
-      @driver.find_elements(:xpath, "//div[normalize-space(text())='Padel']")[1].click
-      return @driver
-    rescue Exception => e
-      puts "Error: #{e.message}"
-    ensure
-    end
-  end
-
-  def self.select_duration(duration: 90)
-    begin
-      puts "Selecting duration..."
-      @driver.find_element(:xpath, "//div[normalize-space(text())='90']").click
-    rescue Exception => e
-      puts "Error: #{e.message}"
-    ensure
-      return nil
-    end
-  end
-
-  def self.select_date(date: Date.tomorrow)
-    begin
-      puts "Selecting date..."
-      dates = @driver.find_elements(xpath: "//div[contains(@class, 'cds-day')]")
-      date_ec = I18n.l(date, format: "%d\n%a.").upcase
-      month = @driver.find_element(xpath: "//div[contains(@class, 'cds-month')]").text
-      availabel_dates = dates.map do |d|
-        day = d.text.split("\n")[0]
-        [Date.parse("#{day}-#{month}"), d.text] 
-      end
-      p availabel_dates
-      dates.last.click
-    rescue Exception => e
-      puts "Error: #{e.message}"
-    ensure
-      return @driver
-    end
-  end
-
-  def self.select_timeslot(start_time: "10:00")
-    timeslots = @driver.find_elements(xpath: "//div[contains(@class, 'hour_item_number')]")
-    if Rails.env.development?
-      timeslots.first.click
-    else
-      timeslot = timeslots.select{|t| t.text == start_time}.first
-      timeslot.click if timeslot
-    end 
-    sleep(2)
-    @driver.find_element(xpath: "//a[normalize-space(text())='Siguiente']").click
-  end
-
-  def self.create_clubs
+  def create_clubs
     file = File.read("./app/bots/easy_cancha_clubs.json")
     clubs = JSON.parse!(file)["clubs"]
 
@@ -179,22 +99,17 @@ class EasycanchaBot
 
   private
 
-  def self.parse_date(date)
-
-  end
-
-  def self.put_separator
+  def put_separator
     puts "****************"
     puts "****************"
     puts "****************"
   end
 
-  def self.wait(timeout = 5000)
+  def wait(timeout = 5000)
     @wait = Selenium::WebDriver::Wait.new(timeout: timeout)
   end
 
-  def self.screenshot
+  def screenshot
     @driver.save_screenshot("./app/assets/images/screenshots/screenshot.png")
   end
 end
-
---
+++ app/bots/easycancha_reservation_service.rb
@@ -0,0 +1,124 @@
+module EasycanchaReservationService
+  def reserve(club_id: 336, starts_at: DateTime.tomorrow, duration: 90)
+    begin
+      login
+      put_separator
+      select_sport(club_id: club_id)
+      sleep(2)
+      select_duration(duration: duration)
+      sleep(3)
+      select_date(date: starts_at.to_date)
+      sleep(2)
+      select_timeslot
+      sleep(2)
+      select_court
+      sleep(2)
+      add_players
+      sleep(4)
+      complete_reservation
+      sleep(12)
+      screenshot
+    rescue Exception => e
+      puts e.message
+    ensure
+      @driver.close
+      @driver.quit
+      put_separator
+    end
+  end
+
+  def select_sport(club_id: 502)
+    begin
+      puts "Selecting sport..."
+      create_driver unless @driver
+      @driver.get "https://www.easycancha.com/book/clubs/#{club_id}/sports?lang=es-CL&country=CL"
+      sleep(2)
+      @driver.find_elements(:xpath, "//div[normalize-space(text())='Padel']")[
+        1
+      ].click
+      return @driver
+    rescue Exception => e
+      puts "Error: #{e.message}"
+    ensure
+    end
+  end
+
+  def select_duration(duration: 90)
+    begin
+      puts "Selecting duration..."
+      @driver.find_element(:xpath, "//div[normalize-space(text())='90']").click
+    rescue Exception => e
+      puts "Error: #{e.message}"
+    ensure
+      return nil
+    end
+  end
+
+  def select_date(date:)
+    begin
+      puts "Selecting date..."
+      dates = @driver.find_elements(xpath: "//div[contains(@class, 'cds-day')]")
+      date_ec = I18n.l(date, format: "%d\n%a.").upcase
+      month =
+        @driver.find_element(xpath: "//div[contains(@class, 'cds-month')]").text
+      availabel_dates =
+        dates.map do |d|
+          day = d.text.split("\n")[0]
+          [Date.parse("#{day}-#{month}"), d.text]
+        end
+      dates.last.click
+    rescue Exception => e
+      puts "Error: #{e.message}"
+    ensure
+      return @driver
+    end
+  end
+
+  def select_timeslot(start_time: "10:00")
+    puts "Selecting timeslot"
+    timeslots =
+      @driver.find_elements(
+        xpath: "//div[contains(@class, 'hour_item_number')]"
+      )
+    if Rails.env.development?
+      timeslots.first.click
+    else
+      timeslot = timeslots.select { |t| t.text == start_time }.first
+      timeslot.click if timeslot
+    end
+    sleep(2)
+    @driver.find_element(
+      xpath: "//a[normalize-space(text())='Siguiente']"
+    ).click
+  end
+
+  def select_court()
+    puts "Selecting court..."
+    @driver.find_element(
+      xpath: "//button[normalize-space(text())='Seleccionar']"
+    ).click
+  end
+
+  def add_players
+    puts "Adding player..."
+    @driver.find_element(
+      xpath: "//button[normalize-space(text())='Agregar / Quitar jugadores']"
+    ).click
+    sleep(2)
+    friends =
+      @driver.find_elements(
+        xpath: "//div[contains(@class, 'row mobileListItemV2 ng-scope')]"
+      )
+    puts "Agregando a #{friends[6].text}"
+    friends[6].click
+    @driver.find_element(
+      xpath: "//button[normalize-space(text())='Seleccionar']"
+    ).click
+  end
+
+  def complete_reservation
+    @driver.find_element(
+      xpath: "//button[contains(@class, 'reserva_btn_terceary')]"
+    ).click
+  end
+end
