12177b5834f55069c2c76c5e5483f78b07281057
Gonzalo Reutter <greutter@gmail.com>
2022-11-30 10:48:19 -0300

    Spinner

--- app/javascript/controllers/spinner_controller.js
+++ app/javascript/controllers/spinner_controller.js
@@ -1,7 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+    static targets = [ "spinnerDiv", "contentDiv" ]
+    
     connect() {
-        console.log("Hello, Stimulus!", this.element)
+        console.log(spinnerDiv.textContent, this.element);
+        spinnerDiv.style.display = "none"
     }
+    show() {
+        contentDiv.style.display = "none" 
+        spinnerDiv.style.display = "block";
+    }
+    hidde() {
+        console.log(spinnerDiv.textContent, this.element);
+        contentDiv.style.display = "block" 
+        spinnerDiv.style.display = "none";
+    }
+
 }
