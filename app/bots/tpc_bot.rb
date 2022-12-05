60555aff927706dfa750760887837bc242e9ae53
Gonzalo Reutter <greutter@gmail.com>
2022-12-05 18:12:10 -0300

    WIP on tpcBot

--- app/bots/tpc_bot.rb
+++ app/bots/tpc_bot.rb
@@ -27,7 +27,7 @@ class TpcBot
 
   def parse_available_timeslots(date: , duration: 90)
     p date
-    # begin
+    begin
       gs = @driver.find_elements(tag_name: "g")
       slots = gs.filter { |g| g.attribute("id").match /ocupacion_/ }
 
@@ -36,7 +36,7 @@ class TpcBot
         y = slot.find_element(tag_name: "rect").attribute("y").to_i
         s = slot.text.split("\n$")
         h = s[0].split "-"
-        price = (s[1].tr(".", "").to_i unless s[1].blank?),
+        price = (s[1].tr(".", "").to_i unless s[1].blank?)
         {
           court: {number: get_court_by(x), price: price},
           starts_at: date.to_time.change_hour_minutes(h[0]),
@@ -44,30 +44,29 @@ class TpcBot
           table_rect_coord: {x: x, y: y}
         }.stringify_keys
       end
-      # available_time_slots = to_available_time_slots(ats)
-      return slots
-      # return 
-    # rescue Exception => e
-    #   p e 
-    # ensure
+      available_time_slots = to_available_time_slots(ats)
+      return available_time_slots
+    rescue Exception => e
+      p e 
+    ensure
       @driver.quit if @driver
-    # end
+    end
   end
   
   def to_available_time_slots(ats)
-    # start_times = start_times_of(ats)
-    # available_time_slots = {}
-    # start_times.each do |start_time|
-    #   slots = ats.filter { |slot| slot["starts_at"] == start_time }
-    #   courts = slots.map { |slot| slot["court"] }
-    #   available_time_slots[start_time] = 
-    #     {
-    #       starts_at: slots.first["starts_at"],
-    #       ends_at: slots.first["ends_at"],
-    #       courts: courts
-    #     }
-    # end
-
+    start_times = start_times_of(ats)
+    available_time_slots = {}
+    start_times.each do |start_time|
+      slots = ats.filter { |slot| slot["starts_at"] == start_time }
+      courts = slots.map { |slot| slot["court"] }
+      available_time_slots[start_time] = 
+        {
+          starts_at: slots.first["starts_at"],
+          ends_at: slots.first["ends_at"],
+          courts: courts
+      }
+    end
+    return available_time_slots.stringify_keys
   end
 
   def start_times_of(ats)
