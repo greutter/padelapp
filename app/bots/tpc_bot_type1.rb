2773aaeb79857551d4dc20c32daa877a172d3410
Gonzalo Reutter <greutter@gmail.com>
2022-12-16 10:45:55 -0300

    super duper2

---
+++ app/bots/tpc_bot_type1.rb
@@ -0,0 +1,46 @@
+module TpcBotType1
+
+  def parse_available_timeslots_type1(date:, duration: 90)
+    gs = @driver.find_elements(tag_name: "g")
+    slots = gs.filter { |g| g.attribute("id").match /ocupacion_/ }
+    available_time_slots = Availability.new_availability_json date
+    if slots.blank?
+      return {}
+    else
+      slots.each do |slot|
+        y_coordinate = slot.find_element(tag_name: "rect").attribute("y").to_i
+        h = slot.text.split("\n$")[0].split("-")
+        court = parse_court_type1(slot)
+        starts_at = date.in_time_zone.change_hour_minutes(h[0])
+        ends_at = starts_at + duration.minutes
+        if available_time_slots[starts_at].blank?
+          available_time_slots[starts_at] = {
+            "starts_at" => starts_at,
+            "ends_at" => ends_at,
+            "y" => y_coordinate,
+            "courts" => [court]
+          }
+        else
+          available_time_slots[starts_at]["courts"] << court
+        end
+      end
+      return available_time_slots
+    end
+  end
+
+  def parse_court_type1(slot)
+    x_coordinate = slot.find_element(tag_name: "rect").attribute("x").to_i
+    court_number = (x_coordinate - 50) / 120 + 1
+    court = club.courts.find_or_create_by(number: court_number)
+    s = slot.text.split("\n$")
+    price = s[1].blank? ? "" : s[1].tr(".", "").to_i
+    return(
+      {
+        court_number: court.number,
+        price: price,
+        court_id: court.id,
+        x_coordinate: x_coordinate
+      }.stringify_keys
+    )
+  end
+end
